202cc19d9fc603b9ca290074188c89d835902b4b
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2020-11-20 14:42:03 +0300

    add variable value

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-let money;
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+let money = 10500;
+let income = 'freelance'; 
+let addExpenses ='Интернет, Такси, Коммуналка, Тренировка, Еда'; 
+let deposit = true; 
+let mission = 150000; 
+let period = 8;
 
-alert('Hello JavaScript');
-console.log('Hello world');
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('addExpenses: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`)
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money / 30;
+console.log('budgetDay:', budgetDay);
